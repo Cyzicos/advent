d25084013ebe25d0379e302b0881e7baf9c328be
hannes <h.vietz@gmail.com>
2018-12-08 11:13:51 +0100

    added dir

---
+++ advent/day1/day1_utils.py
@@ -0,0 +1,51 @@
+import itertools
+
+from advent import io_utils
+
+
+def stage_part1():
+    path = '/home/hannes/repos/advent/day1/input.txt'
+    numbers = load_input(path)
+    print(calc_part1(numbers))
+
+
+def stage_part2():
+    path = '/home/hannes/repos/advent/day1/input.txt'
+    numbers = load_input(path)
+    print(calc_part2(numbers))
+
+
+def calc_part1(numbers):
+    return sum(numbers)
+
+
+def calc_part2(numbers):
+    cycle = itertools.cycle(numbers)
+    cycle = itertools.chain([0], cycle)
+    cycle = itertools.accumulate(cycle)
+    return set_iterator(cycle)
+
+
+def set_iterator(it):
+    accs = set()
+    for i in it:
+        if i in accs:
+            return i
+        else:
+            accs.update([i])
+    return 0
+
+
+def load_input(path):
+    loaded = io_utils.load_txt(path)
+    return clean_input(loaded)
+
+
+def clean_input(_input):
+    _input = [i.strip() for i in _input]
+    _input = [to_int(i) for i in _input]
+    return _input
+
+
+def to_int(str_num):
+    return int(str_num)
--- advent/day1/test_day1.py
+++ advent/day1/test_day1.py
@@ -1,13 +1,5 @@
 # -*- coding: utf-8 -*-
-from day1 import day1_utils
-
-
-def test_foo():
-    assert 1 == 1
-
-
-def test_foo2():
-    assert 1 == 1
+from advent.day1 import day1_utils
 
 
 def test_load():
@@ -26,3 +18,14 @@ def test_to_int_edge():
 
 def test_part1():
     assert day1_utils.calc_part1([1, 2, 3]) == 6
+
+
+def test_part2():
+    assert day1_utils.calc_part2([1, 2, 3]) == 0
+    assert day1_utils.calc_part2([1, 2, 3, -5]) == 1
+    assert day1_utils.calc_part2([1, -1]) == 0
+
+
+def test_set_iterator():
+    assert day1_utils.set_iterator([1, 2, 3]) == 0
+    assert day1_utils.set_iterator([1, 3, 1, -5]) == 1
