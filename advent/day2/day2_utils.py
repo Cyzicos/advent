079806753e92defc24bfb2c0237233611d53ff86
hannes <h.vietz@gmail.com>
2018-12-08 12:47:34 +0100

    code for part 1

--- advent/day2/day2_utils.py
+++ advent/day2/day2_utils.py
@@ -1,7 +1,32 @@
-from advent import io_utils
+import collections
+import itertools
 
-path = '/home/hannes/repos/advent/advent/day2/input.txt'
+from advent import io_utils
 
 
 def is_no_letters(_id):
     return len(_id) == len(set(_id))
+
+
+def do_all(ids):
+    out = (one_id(_id) for _id in ids)
+    acc = itertools.accumulate(out, func=tuple_adder)
+    return collections.deque(acc, maxlen=1).pop()
+
+
+def one_id(_id):
+    counter = collections.Counter(_id)
+    two = 2 in counter.values()
+    three = 3 in counter.values()
+    return two, three
+
+
+def tuple_adder(total, elem):
+    return total[0]+elem[0], total[1]+elem[1]
+
+
+def stage_part1():
+    path = '/home/hannes/repos/advent/advent/day2/input.txt'
+    ids = io_utils.load_txt(path)
+    twos, threes = do_all(ids)
+    return twos*threes
--- advent/day2/test_day2.py
+++ advent/day2/test_day2.py
@@ -2,6 +2,39 @@
 from advent.day2 import day2_utils
 
 
-def check_no_letters():
+def test_no_letters():
     assert day2_utils.is_no_letters('abcdef') is True
     assert day2_utils.is_no_letters('aabcde') is False
+
+
+def test_one_id():
+    out = day2_utils.one_id('abcdef')
+    assert not out[0]
+    assert not out[1]
+
+    out = day2_utils.one_id('abbcdef')
+    assert out[0]
+    assert not out[1]
+
+    out = day2_utils.one_id('abbbcdef')
+    assert not out[0]
+    assert out[1]
+
+    out = day2_utils.one_id('abbbccdef')
+    assert out[0]
+    assert out[1]
+
+
+def test_tuple_adder():
+    total = (1, 2)
+    to_add = (3, 4)
+    added = day2_utils.tuple_adder(total, to_add)
+    assert added[0] == 4
+    assert added[1] == 6
+
+
+def test_do_all():
+    ids = ['abbcd', 'aabcde', 'aaacde']
+    twos, threes = day2_utils.do_all(ids)
+    assert twos == 2
+    assert threes == 1
